a401fce1265351be8a40f773a7a27d723d05a919
Faitus Jeline Joseph <65098339+faitusjelinej@users.noreply.github.com>
2020-06-03 00:06:12 -0700

    Exception program

---
+++ JavaException.java
@@ -0,0 +1,27 @@
+class JavaException
+{
+   public static void main(String args[]){
+     try{
+         int a[]=new int[7];
+         a[4]=30/0;
+         System.out.println("First print statement in try block");
+     }
+     catch(ArithmeticException e)
+	 {
+        System.out.println("Warning: ArithmeticException : " +e);
+     }
+     catch(ArrayIndexOutOfBoundsException e){
+        System.out.println("Warning: ArrayIndexOutOfBoundsException");
+     }
+     catch(Exception e)
+	 {
+        System.out.println("Warning: Some Other exception");
+     }
+ 
+  finally
+	{
+	System.out.println("Inside the finally block");
+	}
+
+  }
+}
